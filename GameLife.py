2b6fb54fc4607f4cfd58de9eb6b90577d4c18a16
kulackov-yaroslaw <47427361+kulackov-yaroslaw@users.noreply.github.com>
2019-04-17 19:20:06 +0300

    Add files via upload
    Зеленая кнопка - запуск автоматической жизни, добавлена возможность устанавливать цвет и количество клеток вокруг данной для того чтоб она ожила.

--- GameLife.py
+++ GameLife.py
@@ -1,25 +1,38 @@
-from tkinter import *
+from tkinter import Tk
+from tkinter import Button
+
 
 SIZE = 20
+run = 0
+
+color_live = 'blue'
+color_die = "red"
+count_cells_around_for_live = 3
+
+
+def change_run():
+    global run
+    run = (run + 1) % 2
+
 
 class But:
     def __init__(self):
         self.button = Button(root)
         self.button.bind("<Button-1>", self.pressed)
-        #self.button.pack()
         self.button["width"] = 1
         self.button["height"] = 1
-
-        self.button["bg"] = "red"
+        self.button["bg"] = color_die
         self.alive = 0
+
     def pressed(self, event):
         self.alive = (self.alive  + 1) % 2
         if self.alive == 1:
-            self.button["bg"] = "blue"
+            self.button["bg"] = color_live
         else:
-            self.button["bg"] = "red"
+            self.button["bg"] = color_die
 
-def rpressed(event):
+
+def rpressed(move):
     buffer = []
     for i in range(1, SIZE - 1):
         for j in range(1, SIZE - 1):
@@ -40,10 +53,10 @@ def rpressed(event):
                 s += 1
             if koords[i - 1][j - 1].alive == 1:
                 s += 1
-            if koords[i][j].alive == 1 and (s < 2 or s > 3):
-                buffer.append([i,j])
-            if koords[i][j].alive == 0 and s == 3:
-                buffer.append([i,j])
+            if koords[i][j].alive == 1 and (s < count_cells_around_for_live  - 1 or s > count_cells_around_for_live):
+                buffer.append([i, j])
+            if koords[i][j].alive == 0 and s == count_cells_around_for_live:
+                buffer.append([i, j])
     for i in buffer:
         koords[i[0]][i[1]].pressed("<Button-1>")
 
@@ -54,6 +67,12 @@ root.bind("<Button-3>", rpressed)
 koords = [[But() for i in range(SIZE)] for j in range(SIZE)]
 for i in range(SIZE):
     for j in range(SIZE):
-        koords[i][j].button.grid(row = i, column = j)
+        koords[i][j].button.grid(row=i, column=j)
+btn = Button(command=change_run, bg="green")
+btn["width"] = 1
+btn.grid(row=0, column=0)
 
-root.mainloop()
+while True:
+    if run:
+        rpressed(1)
+    root.update()
